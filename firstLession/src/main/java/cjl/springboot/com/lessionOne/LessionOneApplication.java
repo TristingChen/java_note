06a058b6947bebd5239ff21b095bfe0da44a5ed4
chenjialing <chenjialing@aspirecn.com>
2019-10-31 18:14:51 +0800

    springboot首次下载代码运行成功

---
+++ firstLession/src/main/java/cjl/springboot/com/lessionOne/LessionOneApplication.java
@@ -0,0 +1,13 @@
+package cjl.springboot.com.lessionOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessionOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LessionOneApplication.class, args);
+	}
+
+}
---
+++ firstLession/src/test/java/cjl/springboot/com/lessionOne/LessionOneApplicationTests.java
@@ -0,0 +1,13 @@
+package cjl.springboot.com.lessionOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessionOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
