aba7cacaf6b7fd8638d4c46d1719b9177a325569
chenjialing <chenjialing@aspirecn.com>
2019-12-13 14:09:27 +0800

    增加user返回user类，测试jackson自己处理json的转化

---
+++ cjl_java1/src/main/java/com/cjl/java/cjl_java/CjlJavaApplication.java
@@ -0,0 +1,13 @@
+package com.cjl.java.cjl_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CjlJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CjlJavaApplication.class, args);
+    }
+
+}
---
+++ cjl_java1/src/test/java/com/cjl/java/cjl_java/CjlJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cjl.java.cjl_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CjlJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
